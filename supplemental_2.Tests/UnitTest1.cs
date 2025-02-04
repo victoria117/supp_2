db62bd17d61aa28b06c997c617dd673c109b47a1
victoria117 <1107faith@gmail.com>
2025-02-04 01:31:51 -0500

    Documentation added for colo generator function

--- supplemental_2.Tests/UnitTest1.cs
+++ supplemental_2.Tests/UnitTest1.cs
@@ -1,32 +1,10 @@
-namespace supplemental_2.Tests;
+﻿namespace supplemental_2.Tests;
 
 public class UnitTest1
 {
-        int length = 8;
-        string password = RandomUtils.GeneratePassword(length);
-
-        Assert.Equal(length, password.Length);
-        Assert.Matches(@"^[A-Za-z0-9_]+$", password);
-    }
-
-
     [Fact]
     public void Test1()
     {
-        var (hex, (r, g, b)) = RandomUtils.GenerateRandomColor();
 
-        Assert.Matches(@"^#[0-9A-Fa-f]{6}$", hex);
-        Assert.InRange(r, 0, 255);
-        Assert.InRange(g, 0, 255);
-        Assert.InRange(b, 0, 255);
-
-        string expectedHex = $"#{r:X2}{g:X2}{b:X2}";
-        Assert.Equal(expectedHex, hex);
     }
 }
-
-
-
-
-
-

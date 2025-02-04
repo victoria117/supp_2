5ffaaabc4bf6fab80e9b1315605ca1fc7867ad42
victoria117 <1107faith@gmail.com>
2025-02-04 01:15:40 -0500

    Added documentation for normal distribution function

--- supplemental_2.Tests/UnitTest1.cs
+++ supplemental_2.Tests/UnitTest1.cs
@@ -1,28 +1,10 @@
-namespace supplemental_2.Tests;
+﻿namespace supplemental_2.Tests;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-        double mean = 50;
-        double stdDev = 10;
-        int sampleSize = 10000;
-        double sum = 0;
-        double sumSq = 0;
 
-        for (int i = 0; i < sampleSize; i++)
-        {
-            double value = RandomUtils.GenerateNormal(mean, stdDev);
-            sum += value;
-            sumSq += value * value;
-        }
-
-        double sampleMean = sum / sampleSize;
-        double sampleVariance = (sumSq / sampleSize) - (sampleMean * sampleMean);
-        double sampleStdDev = Math.Sqrt(sampleVariance);
-
-        Assert.InRange(sampleMean, mean - 1, mean + 1);
-        Assert.InRange(sampleStdDev, stdDev - 1, stdDev + 1);
     }
 }
--- supplemental_2/supplemental_2.cs
+++ supplemental_2/supplemental_2.cs
@@ -1,35 +1,8 @@
-using 
+﻿using 
 
 namespace supplemental_2;
 
 public class supplemental_2
 {
-            double u1 = 1.0 - random.NextDouble();
-            double u2 = 1.0 - random.NextDouble();
-            double standardNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
-            return mean + stdDev * standardNormal;
-        }
-public static string GeneratePassword(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
-            StringBuilder password = new StringBuilder(length);
-            for (int i = 0; i < length; i++)
-            {
-                password.Append(chars[random.Next(chars.Length)]);
-            }
-            return password.ToString();
-        }
 
-
-public static (string, (int, int, int)) GenerateRandomColor()
-        {
-            int r = random.Next(256);
-            int g = random.Next(256);
-            int b = random.Next(256);
-            string hex = $"#{r:X2}{g:X2}{b:X2}";
-            return (hex, (r, g, b));
-        }
-    }
 }
-
-

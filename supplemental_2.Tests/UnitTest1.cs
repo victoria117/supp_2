b26292d4967c7575f68a248218db50eae00fcd87
victoria117 <1107faith@gmail.com>
2025-02-04 01:20:22 -0500

    Test for password generator

--- supplemental_2.Tests/UnitTest1.cs
+++ supplemental_2.Tests/UnitTest1.cs
@@ -1,10 +1,22 @@
-﻿namespace supplemental_2.Tests;
+namespace supplemental_2.Tests;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        int length = 8;
+        string password = RandomUtils.GeneratePassword(length);
 
+        Assert.Equal(length, password.Length);
+        Assert.Matches(@"^[A-Za-z0-9_]+$", password);
     }
+
+
+
+
+
+
+
+
 }
